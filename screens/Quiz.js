f8fee5e60ed6e6a2725e29c17f10421c1dee7638
Danish Hussain <danishhussain2000@gmail.com>
2023-02-03 17:32:07 +0530

    update

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View,Text, TouchableOpacity,StyleSheet,ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
 
 
 function shuffleArray(array) {
@@ -9,163 +9,171 @@ function shuffleArray(array) {
     }
 }
 
-function Quiz({navigation}) {
-    const[questions,setQuestions]=useState()
-    const[ques,setQues]=useState(0)
-    const[options,setOptions]=useState([])
-    const[score,setScore]=useState(0)
-    const[isLoading,setIsLoading]=useState(false)
-    const getQuiz=async()=> {
+function Quiz({ navigation }) {
+    const [questions, setQuestions] = useState()
+    const [ques, setQues] = useState(0)
+    const [options, setOptions] = useState([])
+    const [score, setScore] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const getQuiz = async () => {
         setIsLoading(true)
         let response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
-          let json = await response.json();
-        //   console.log(json.results)
-          setQuestions(json.results)
-          setOptions(genrateOptionsAndShuffles(json.results[0]))
+        let json = await response.json();
+        console.log(json.results)
+        setQuestions(json.results)
+        setOptions(genrateOptionsAndShuffles(json.results[0]))
         setIsLoading(false)
-
     }
-    useEffect(()=>{
+    useEffect(() => {
         getQuiz()
-    },[])
-   
-    const handleNextPress=()=> {
-        setQues(ques+1)
-        setOptions(genrateOptionsAndShuffles(questions[ques+1]))
+    }, [])
+
+    const handleNextPress = () => {
+        setQues(ques + 1)
+        setOptions(genrateOptionsAndShuffles(questions[ques + 1]))
 
     }
 
-    let genrateOptionsAndShuffles =(_question)=> {
-    let options=[..._question.incorrect_answers]
-    options.push(_question.correct_answer)
-    shuffleArray(options)
-    return options
+    let genrateOptionsAndShuffles = (_question) => {
+        let options = [..._question.incorrect_answers]
+        options.push(_question.correct_answer)
+        shuffleArray(options)
+        return options
     }
-    const handleSelectedOption=(_option)=> {
-        if (_option===questions[ques].correct_answer) {
-            setScore(score+10)
+    const handleSelectedOption = (_option) => {
+        if (_option === questions[ques].correct_answer) {
+            setScore(score + 10)
         }
-        if (ques!==9) {
-            setQues(ques+1)
-        setOptions(genrateOptionsAndShuffles(questions[ques+1]))
+        if (ques !== 9) {
+            setQues(ques + 1)
+            setOptions(genrateOptionsAndShuffles(questions[ques + 1]))
         }
-        if (ques===9) {
+        if (ques === 9) {
             handleShowResult()
         }
-        
+
     }
-    const handleShowResult=()=> {
-        navigation.navigate('Result',{score:score})
+    const handleShowResult = () => {
+        navigation.navigate('Result', { score: score })
     }
- 
-    return ( 
+    // console.log(questions[ques].question)
+    // console.log(questions[ques].correct_answer)
+    return (
         <View style={styles.container}>
-            {isLoading?<View style={{flex:1,alignItems:'center' , justifyContent:'center',}}>
+            {isLoading ? <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
                 <ActivityIndicator size="large" />
-                <Text style={{fontSize:25, fontWeight:'400'}}>Loading</Text></View>:questions &&
+                <Text style={{ fontSize: 25, fontWeight: '400' }}>Loading</Text></View> : questions &&
             <View style={styles.parent}>
-            <View style={styles.top}>
-            <Text style={styles.question}>Q.{ques+1}:{decodeURIComponent( questions[ques].question)}</Text>
-            </View>
-            <View style={styles.options}>
-                <TouchableOpacity style={styles.textOption} onPress={()=>handleSelectedOption(options[0])}>
-                <Text style={styles.option}>{decodeURIComponent(options[0])}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.textOption} onPress={()=>handleSelectedOption(options[1])}>
-                <Text style={styles.option}>{decodeURIComponent(options[1])}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.textOption} onPress={()=>handleSelectedOption(options[2])}>
-                <Text style={styles.option}>{decodeURIComponent(options[2])}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.textOption} onPress={()=>handleSelectedOption(options[3])}>
-                <Text style={styles.option}>{decodeURIComponent(options[3])}</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.bottom}>
-            
+                <View style={styles.top}>
+                    <Text style={styles.question}>Q{ques + 1}: {decodeURIComponent(questions[ques].question)}</Text>
+                </View>
+                <View style={styles.options}>
+                    <TouchableOpacity style={styles.textOption} onPress={() => handleSelectedOption(options[0])}>
+                        <Text style={styles.option}>{decodeURIComponent(options[0])}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.textOption} onPress={() => handleSelectedOption(options[1])}>
+                        <Text style={styles.option}>{decodeURIComponent(options[1])}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.textOption} onPress={() => handleSelectedOption(options[2])}>
+                        <Text style={styles.option}>{decodeURIComponent(options[2])}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.textOption} onPress={() => handleSelectedOption(options[3])}>
+                        <Text style={styles.option}>{decodeURIComponent(options[3])}</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.bottom}>
 
-                {ques!==9 && <TouchableOpacity onPress={()=>handleNextPress()} style={styles.button}>
-                <Text style={styles.buttonText}>Next</Text>
-                </TouchableOpacity>}
 
-                {ques===9 && <TouchableOpacity onPress={()=>handleShowResult()} style={styles.button}>
-                <Text style={styles.buttonText}>Result</Text>
-                </TouchableOpacity>}
+                    {ques !== 9 && <TouchableOpacity onPress={() => handleNextPress()} style={styles.button}>
+                        <Text style={styles.buttonText}>Next</Text>
+                    </TouchableOpacity>}
+
+                    {ques === 9 && <TouchableOpacity onPress={() => handleShowResult()} style={styles.button}>
+                        <Text style={styles.buttonText}>Result</Text>
+                    </TouchableOpacity>}
+                </View>
             </View>
-            </View>
-             } 
+            }
         </View>
-     );
+    );
 }
 
 export default Quiz;
 const styles = StyleSheet.create({
     container: {
         padding: 12,
-        flex:1,
+        flex: 1,
         // backgroundColor:"red"
     },
     top: {
-        marginVertical:8,
-        height:100,
-        // backgroundColor:"red"
+        marginVertical: 8,
+        height: 100,
+        // backgroundColor: "blue",
+        // flex:1
 
     },
     options: {
-        marginVertical:16,
-        flex:1,
+        marginVertical: 16,
+        flex: 1,
         borderWidth: 1,
-        borderColor:"black",
-        borderRadius:10,
-        padding:10,
+        borderColor: "black",
+        borderRadius: 10,
+        padding: 10,
         // backgroundColor:"blue",
-        
+
     },
     bottom: {
-        marginBottom:12,
-        paddingVertical:16,
-        justifyContent:'space-between',
-        flexDirection:'row',
-    
+        marginBottom: 12,
+        paddingVertical: 16,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+
     },
     textOption: {
-        borderWidth:1,
-        marginVertical:16,
-        marginHorizontal:8,
-        padding:30,
-        margin:60,
-        borderRadius:15,
-        backgroundColor:'#34A0A4'
+        borderWidth: 1,
+        marginVertical: 16,
+        marginHorizontal: 8,
+        padding: 30,
+        margin: 60,
+        borderRadius: 15,
+        backgroundColor: '#34A0A4',
+        flex: 1
 
 
     },
     button: {
-        width:'45%',
-        backgroundColor:'#1A759F',
-        padding:16,
-        borderRadius:16,
-        borderColor:'grey',
-        alignItems:"center",
-        marginVertical:10,
+        width: '45%',
+        backgroundColor: '#1A759F',
+        padding: 16,
+        borderRadius: 16,
+        borderColor: 'grey',
+        alignItems: "center",
+        marginVertical: 10,
 
     },
     buttonText: {
-        fontSize:20,
-        fontWeight:'600',
-        color:'white',
+        fontSize: 20,
+        fontWeight: '600',
+        color: 'white',
     },
     question: {
-        fontSize:21,
-        color:'black',
+        fontSize: 21,
+        color: 'black',
+        fontWeight: '600'
         // backgroundColor:"red"
 
     },
     option: {
-        fontSize:16,
-        color:'white'
+        fontSize: 18,
+        color: 'white',
+        fontWeight: '500'
+
     },
     parent: {
-        height:'100%'
+        // height: '100%',
+        flex: 1,
+        // backgroundColor:'green'
     }
 })
 
--- screens/Result.js
+++ screens/Result.js
@@ -1,84 +1,100 @@
 import React from 'react';
-import { View,Text,StyleSheet,Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button } from 'react-native';
 
-function Result({navigation,route}) {
-                
-// const params = route.params
-//     console.log(params)
-const {score} = route.params
+function Result({ navigation, route }) {
+
+    // const params = route.params
+    //     console.log(params)
+    const { score } = route.params
     console.log(score)
-const resultBanner= score>40?'https://img.freepik.com/free-vector/golden-winners-cup_1284-18399.jpg?w=740&t=st=1668533793~exp=1668534393~hmac=7ab0cf696307c163872d5f0df2daa0b75a64225c5e6849cb9d6fc0bb213100d3':
-'https://img.freepik.com/free-photo/debt-ridk-difficulty-downfall-concept_53876-120077.jpg?w=826&t=st=1668534239~exp=1668534839~hmac=8deeb9719798066fe956a7cd101772ba54cea399fad65e4fc5ac754cdecfcfc'
-    return ( 
+    const resultBanner = score > 40 ? 'https://img.freepik.com/free-vector/golden-winners-cup_1284-18399.jpg?w=740&t=st=1668533793~exp=1668534393~hmac=7ab0cf696307c163872d5f0df2daa0b75a64225c5e6849cb9d6fc0bb213100d3' :
+        'https://img.freepik.com/free-photo/businessman-doing-bad-signal_1368-8842.jpg?w=740&t=st=1675424869~exp=1675425469~hmac=25fa62cfc8b46ccdaa01db6550477126bf964095713e7411afd8b30062eabaf3'
+    return (
         <View style={styles.Container}>
-            <View style={styles.result}><Text style={styles.resultText}>Result</Text></View>
-            <View style={styles.result}><Text style={styles.resultTextScore}>{score}</Text></View>
+            <View style={styles.result}>
+                <Text style={styles.resultText}>Result</Text>
+            </View>
+            
+            <View style={styles.result}>
+                <Text style={styles.resultTextScore}>{score}</Text>
+                {score > 40 ? <Text style={{color:"green",fontSize:26,fontWeight:'500'}}>Won</Text>: 
+                <Text style={{color:"red",fontSize:26,fontWeight:'500'}}>Lost</Text>}
+               
+
+            </View>
+            {/* images */}
             <View style={styles.bannerContainer}>
-            <Image source={{uri:resultBanner}}
-                style={styles.banner}
-                resizeMode="contain"
-                />
+                <Image source={{ uri: resultBanner }} style={styles.banner} resizeMode="contain" />
             </View>
             <View>
-                <TouchableOpacity onPress={()=>navigation.navigate('Home')} style={styles.button}>
-                    <Text style={styles.buttonText}>Home</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
+                    {/* <Text style={styles.buttonText}>Try Again</Text> */}
+                    {/* <Button title='Try Again'/> */}
+                    <Text style={{ justifyContent: 'center', alignItems: 'center', alignSelf: 'center', color: 'white', fontSize: 25, fontWeight: '600', }}>Try Again</Text>
                 </TouchableOpacity>
             </View>
         </View>
-     );
+    );
 }
 
 export default Result;
 const styles = StyleSheet.create({
     Container: {
-        backgroundColor:'white',
-        flex:1,
+        backgroundColor: 'white',
+        flex: 1,
         // backgroundColor:'blue'
 
     },
     banner: {
-        height: 450,
-        width:450,
+        height: '100%',
+        width: '100%',
         // marginTop:10,
         // marginBottom:"50%",
-        // backgroundColor:'red'
-
+        // flex:1,
+        bottom: 40,
+        // backgroundColor: 'red'
     },
     bannerContainer: {
-        justifyContent:'center',
-        alignItems:'center',
-        flex:1,
-        // backgroundColor:'green'
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        // backgroundColor: 'green'
 
     },
     button: {
-       marginHorizontal:10,
-        backgroundColor:'#1A759F',
-        padding:16,
-        borderRadius:16,
-        marginBottom:30,
-        
+        marginHorizontal: 10,
+        backgroundColor: '#1A759F',
+        padding: 16,
+        borderRadius: 16,
+        marginBottom: 30,
+        justifyContent: 'center',
+        // alignSelf:'center'
+
+
     },
     buttonText: {
-        fontSize:25,
-        fontWeight:'600',
-        color:'white',
-        paddingLeft:'40%',
+        fontSize: 25,
+        fontWeight: '600',
+        color: 'white',
+        paddingLeft: '40%',
+        alignSelf: 'center'
     },
     result: {
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop:20
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+
     },
     resultText: {
-        fontSize:50,
-        fontWeight:'600',
-        color:'grey',
+        fontSize: 50,
+        fontWeight: '600',
+        color: '#1A759F',
     },
     resultTextScore: {
-        fontSize:30,
-        fontWeight:'400',
-        color:'grey',
+        fontSize: 30,
+        fontWeight: '500',
+        color: 'green',
+
         // backgroundColor:'yellow'
     }
 })
